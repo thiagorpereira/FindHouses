83eb1159325912601e2444f8c33675a03b7cb1ba
Thiago Pereira <thiagorpereira7@gmail.com>
2021-04-30 08:53:09 -0300

    More Text atom

--- src/components/atoms/Text/index.stories.js
+++ src/components/atoms/Text/index.stories.js
@@ -3,8 +3,14 @@ import { text } from '@storybook/addon-knobs';
 import { storiesOf } from '@storybook/react-native';
 import React from 'react';
 import {
-  Title, CardDescription, CardHightLightText, CardTitle,
-  DetailSectionTitle, DetailSubtitle, DetailText, DetailTitle,
+  Title,
+  CardDescription,
+  CardHightLightText,
+  CardTitle,
+  DetailSectionTitle,
+  DetailSubtitle,
+  DetailText,
+  DetailTitle,
   InputLabel,
 } from './index';
 
@@ -17,54 +23,53 @@ const Wrapper = styled.View`
 
 const stories = storiesOf('Text', module);
 
-stories.addDecorator((getStory) => <Wrapper>{getStory()}</Wrapper>)
-
+stories.addDecorator(getStory => <Wrapper>{getStory()}</Wrapper>);
 
 stories.add('Title', () => {
   const value = text('Title', 'Exemplo de Titulo');
   return <Title>{value}</Title>;
-})
+});
 
 stories.add('InputLabel', () => {
   const value = text('InputLabel', 'Exemplo de InputLabel');
   return <InputLabel>{value}</InputLabel>;
-})
+});
 
 // Detail Texts
 
 stories.add('DetailTitle', () => {
   const value = text('DetailTitle', 'Exemplo de Titulo Detail');
   return <DetailTitle>{value}</DetailTitle>;
-})
+});
 
 stories.add('DetailSectionTitle', () => {
   const value = text('DetailSectionTitle', 'Exemplo de Titulo Detail');
   return <DetailSectionTitle>{value}</DetailSectionTitle>;
-})
+});
 
 stories.add('DetailSubtitle', () => {
   const value = text('DetailSubtitle', 'Exemplo de Titulo Detail');
   return <DetailSubtitle>{value}</DetailSubtitle>;
-})
+});
 
 stories.add('DetailText', () => {
   const value = text('DetailText', 'Exemplo DetailText');
   return <DetailText>{value}</DetailText>;
-})
+});
 
 // Card Texts
 
 stories.add('CardTitle', () => {
   const value = text('CardTitle', 'Exemplo de Titulo Cartão');
   return <CardTitle>{value}</CardTitle>;
-})
+});
 
 stories.add('CardDescription', () => {
   const value = text('CardDescription', 'Exemplo de Descrição');
   return <CardDescription>{value}</CardDescription>;
-})
+});
 
 stories.add('CardHightLightText', () => {
   const value = text('CardHightLightText', 'U$ 222.22');
   return <CardHightLightText>{value}</CardHightLightText>;
-})
+});
