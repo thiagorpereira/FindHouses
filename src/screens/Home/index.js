7d0f0cc9a11b99f146c5303617881418b7406ca1
Thiago Pereira <thiagorpereira7@gmail.com>
2021-04-30 08:55:18 -0300

    Join atom and molecule in Home

---
+++ src/screens/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Title, IconButton, Input } from '../../components';
+import { ScreenContainer, TopContainer, TitleContainer } from './styles';
+
+export const HomeScreen = () => {
+  return (
+    <ScreenContainer>
+      <TopContainer>
+        <TitleContainer>
+          <Title>Encontre aqui seu imóvel</Title>
+        </TitleContainer>
+        <IconButton iconName="filter" />
+      </TopContainer>
+      <Input label="Localização" placeholder="Digite o Endereço" />
+    </ScreenContainer>
+  );
+};
